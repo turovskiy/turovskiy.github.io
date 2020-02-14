580549fc7ee8a5de0d588f70f0548b09c79a6d34
turovskiy <47497087+turovskiy@users.noreply.github.com>
2020-02-14 12:02:52 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+$(function(){
+
+        $("#typing").typed({
+            strings: [" ІНТЕРНЕТ", " ТЕЛЕБАЧЕННЯ", " ВІДЕОНАГЛЯД"],
+            typeSpeed: 70,
+            backDelay: 1500,
+            startDelay: 2500,
+            loop: true,
+            loopCount: 2,
+            contentType: 'html',      
+        });
+
+    });
